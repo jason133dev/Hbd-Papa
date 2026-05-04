594c29cf70e272fe865b8dbd969c57548861684d
jason133.dev <jason133.dev@gmail.com>
2026-05-04 13:18:48 +0700

    Tambah lagu

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let awal = document.querySelector(`.awal`);
 let container = document.querySelector(`.container`);
-let lagu = new Audio(`sorry.mp3`);
+let lagu = new Audio(`NIKI - You'll Be In My Heart _ Lirik Terjemahan Indonesia.mp3`);
 
 function tampil() {
     setTimeout(() => {
         awal.style.pointerEvents = `none`;
         awal.style.opacity = `0`;
         lagu.play();
-        lagu.currentTime = `3.5`;
+        lagu.currentTime = `0gi`;
         lagu.volume = `1`;
     }, 100);
 }
