908163ea6084e7910aab7a19f0c69b291cc630fd
jason133.dev <jason133.dev@gmail.com>
2026-05-04 13:17:37 +0700

    Selesai

--- main.js
+++ main.js
@@ -3,20 +3,13 @@ let container = document.querySelector(`.container`);
 let lagu = new Audio(`sorry.mp3`);
 
 function tampil() {
-    awal.style.opacity = `0`;
-    awal.style.visibility = `hidden`;
-    awal.style.pointerEvents = `none`;
-    container.style.display = `flex`;
-    
-
     setTimeout(() => {
-        awal.style.display = `none`;
-        container.style.opacity = `1`;
-        container.style.visibility = `visible`;
+        awal.style.pointerEvents = `none`;
+        awal.style.opacity = `0`;
         lagu.play();
         lagu.currentTime = `3.5`;
         lagu.volume = `1`;
-    }, 500);
+    }, 100);
 }
 
 awal.addEventListener(`click`, tampil);
